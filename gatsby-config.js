2972396f4c4ea6dfc4587f4d3c1102b84fafd80a
Leandro Pessini <leandro@pessini.me>
2023-10-16 09:27:26 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-site-example`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
